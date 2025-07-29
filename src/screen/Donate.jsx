3feb2e74940b06fa45f90ab398f55f6c20504064
Zaki <zaki@Zakis-MacBook-Air.local>
2025-07-29 09:29:25 +0500

    immplement react form hook for validation but still not completed

--- src/screen/Donate.jsx
+++ src/screen/Donate.jsx
@@ -1,138 +1,218 @@
 import React, { useState } from "react";
 import InputField from "../components/InputField";
 import { bloodGroups } from "../constant/blood_group";
+import { useForm } from "react-hook-form";
+import * as yup from 'yup'
+import { yupResolver } from "@hookform/resolvers/yup";
+
 
 function Donate() {
-  const [stepper, SetStepper] = useState(1);
+  const yupSchema = yup.object({
+    name: yup.string().required("Enter Your Name").min(2, 'Your name must be 2 charaters'),
+    email: yup.string().required("Enter Your Email").email(),
+    number: yup.string().required("Enter Your Mobile Number").min(11, 'Your Mobile Number must be 11 charaters'),
+    province: yup.string().required("Enter Your Province"),
+    district: yup.string().required("Enter Your District"),
+    location: yup.string().required('Enter Your Location').min(3, 'Enter a valid Location'),
+    selectBloodGroup: yup.string().required("Please Enter Your Blood Group")
+  }).required()
+  const [stepper, setStepper] = useState(1);
+  const [selectedBlood, setSelectedBlood] = useState()
+  const { register, setValue, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(yupSchema) })
+
+
+  const nextStepper = () => {
+    if (stepper < 3) {
+      setStepper((prev) => prev + 1);
+    }
+  };
+
+  const prevStepper = () => {
+    if (stepper > 1) {
+      setStepper((prev) => prev - 1);
+    }
+  };
+
+  const userSelectedBlood = (item) => {
+    console.log(item)
+    setSelectedBlood(item.id)
+    setValue('selectBloodGroup', item.id, { shouldValidate: true })
+  }
+  const onSubmitData = (data)=>{
+    console.log('---->>>',data)
+  }
+
+
   return (
-    <div className=" border-2 border-green-900 flex justify-center items-center ">
-      <div className=" border-2 h-[400px] w-[400px] rounded-[30px] ">
-        <div className=" text-center text-[23px] mt-2 ">
-          <strong> Enter Your Details </strong>
-        </div>
-        <label htmlFor="name">
-          <strong className=" text-[17px] ml-3 ">Name:</strong>
-        </label>
-        <InputField
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Enter Your Name"
-          className=" border-2 h-[40px] w-full  pl-2 border-gray-500 focus:border-gray-600 outline-none rounded-[20px] mb-3 "
-        />
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="border-2 border-green-900 w-[400px] min-h-[400px] rounded-[30px] p-6 flex flex-col justify-between bg-white shadow-lg">
 
-        <label htmlFor="number">
-          <strong className=" text-[17px] ml-3  ">Mobile Number:</strong>
-        </label>
-        <InputField
-          type="number"
-          id="number"
-          name="number"
-          placeholder="0300 xxxxxxx"
-          className=" border-2 h-[40px] w-full  pl-2 border-gray-500 focus:border-gray-600 outline-none rounded-[20px] mb-3 "
-        />
 
-        <label htmlFor="email">
-          <strong className=" text-[17px] ml-3 ">E-mail:</strong>
-        </label>
-        <InputField
-          type="text"
-          id="email"
-          name="email"
-          placeholder="example@gmail.com"
-          className=" border-2 h-[40px] w-full  pl-2 border-gray-500 focus:border-gray-600 outline-none rounded-[20px] "
-        />
-
-        <div className="mb-4">
-          <label
-            htmlFor="province"
-            className="block text-lg font-semibold text-gray-700 mb-2 ml-1"
-          >
-            Province
-          </label>
-
-          <div>
-            <select
-              name="province"
-              id="province"
-              className=" border-2 h-[40px] w-full  mt-1 pl-2 border-gray-400 focus:border-gray-600 outline-none rounded-[20px] mb-3 "
-            >
-              <option value="punjab">Punjab</option>
-              <option value="sindh">Sindh</option>
-              <option value="balochistan">Balochistan</option>
-              <option value="kpk">Khyber Pakhtunkhwa</option>
-            </select>
-          </div>
+        <form onSubmit={handleSubmit(onSubmitData)} ><div className="text-center text-[23px] mb-4">
+          <strong>Enter Your Details</strong>
         </div>
 
-        <div className="mb-4">
-          <label
-            htmlFor="district"
-            className="block text-lg font-semibold text-gray-700 mb-2 ml-1"
-          >
-            District
-          </label>
+          <div className="flex-grow overflow-y-auto space-y-4">
+            {stepper === 1 && (
+              <div className="space-y-3">
+                <div>
 
-          <div>
-            <select
-              name="district"
-              id="district"
-              className="w-full  px-4 py-2 border-2 shadow-sm focus:outline-none border-gray-400 focus:border-gray-600 outline-none rounded-[20PX]  text-gray-700"
-            >
-              <option value="punjab">Punjab</option>
-              <option value="sindh">Sindh</option>
-              <option value="balochistan">Balochistan</option>
-              <option value="kpk">Khyber Pakhtunkhwa</option>
-            </select>
-          </div>
-        </div>
-
-        <label htmlFor="location">
-          <label className="block text-lg font-semibold text-gray-700 mb-2 ml-1  ">
-            Your Current Location:
-          </label>
-        </label>
-        <InputField
-          type="text"
-          id="location"
-          name="location"
-          placeholder="Township Butt Chowk Mateen Avenue 404Z"
-          className=" border-2 h-[40px] w-full  mt-1 pl-2 border-gray-400 focus:border-gray-600 outline-none rounded-[20px] mb-3 "
-        />
+                  <label htmlFor="name" className="text-[17px] ml-2 font-semibold">
+                    Name:
+                  </label>
+                  <InputField
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Enter Your Name"
+                    className="border-2 h-[40px] w-full pl-2 border-gray-500 focus:border-gray-600 outline-none rounded-[20px]"
+                    {...register('name')}
+                  />
+                </div>
 
 
+                <div>
+                  <label htmlFor="email" className="text-[17px] ml-2 font-semibold">
+                    E-mail:
+                  </label>
+                  <InputField
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="example@gmail.com"
+                    {...register('email')}
+                    className="border-2 h-[40px] w-full pl-2 border-gray-500 focus:border-gray-600 outline-none rounded-[20px]"
+                  />
+                </div>
 
-        <div className=" border-2 border-green-950 flex mt-5 flex-wrap gap-5 justify-evenly w-full  " >
-          {
-            bloodGroups.map((item)=>(
-              <div className=" border-2 h-[90px] w-[90px] " key={item.id} >
 
-                <img src={item.path} alt={item.name} />
+                <div>
+                  <label htmlFor="number" className="text-[17px] ml-2 font-semibold">
+                    Mobile Number:
+                  </label>
+                  <InputField
+                    type="number"
+                    id="number"
+                    name="number"
+                    placeholder="0300 xxxxxxx"
+                    {...register('number')}
+                    className="border-2 h-[40px] w-full pl-2 border-gray-500 focus:border-gray-600 outline-none rounded-[20px]"
+                  />
+                </div>
+
+
+
+
+
               </div>
-            ))
+            )}
+
+            {stepper === 2 && (
+              <div className="space-y-4">
+                <div>
+                  <label
+                    htmlFor="province"
+                    className="block text-lg font-semibold text-gray-700 ml-1"
+                  >
+                    Province
+                  </label>
+                  <select
+                    name="province"
+                    id="province"
+                    {...register('province')}
+                    className="border-2 h-[40px] w-full pl-2 border-gray-400 focus:border-gray-600 outline-none rounded-[20px]"
+                  >
+                    <option value={null}>Select Your Province</option>
+                    <option value="punjab">Punjab</option>
+                    <option value="sindh">Sindh</option>
+                    <option value="balochistan">Balochistan</option>
+                    <option value="kpk">Khyber Pakhtunkhwa</option>
+                  </select>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="district"
+                    className="block text-lg font-semibold text-gray-700 ml-1"
+                  >
+                    District
+                  </label>
+                  <select
+                    name="district"
+                    id="district"
+                    className="w-full px-4 py-2 border-2 border-gray-400 focus:border-gray-600 outline-none rounded-[20px] text-gray-700"
+                    {...register('district')}
+                  >
+                    <option value={null}>Select Your District</option>
+                    <option value="punjab">Punjab</option>
+                    <option value="sindh">Sindh</option>
+                    <option value="balochistan">Balochistan</option>
+                    <option value="kpk">Khyber Pakhtunkhwa</option>
+                  </select>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="location"
+                    className="block text-lg font-semibold text-gray-700 ml-1"
+                  >
+                    Your Current Location:
+                  </label>
+                  <InputField
+                    type="text"
+                    id="location"
+                    name="location"
+                    placeholder="Township Butt Chowk Mateen Avenue 404Z"
+                    {...register('location')}
+                    className="border-2 h-[40px] w-full pl-2 border-gray-400 focus:border-gray-600 outline-none rounded-[20px]"
+                  />
+                </div>
+              </div>
+            )}
+
+            {stepper === 3 && (
+              <div className="space-y-4">
+                <div className="text-lg font-semibold text-gray-800 text-center">
+                  Select Your Blood Group
+                </div>
+                <div className="flex flex-wrap gap-4 justify-center">
+                  {bloodGroups.map((item) => (
+                    <div
+                      key={item.id}
+                      className={` rounded-xl h-[90px] w-[90px] flex items-center justify-center hover:border-green-500 transition cursor-pointer ${selectedBlood === item.id ? "border-2 border-green-600 " : " border-2 border-gray-300 "} `}
+                      onClick={() => userSelectedBlood(item)}
+                    >
+                      <img src={item.path} alt={item.name} className=" h-full w-full rounded-xl " />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className="flex justify-between items-center mt-6">
+            <button
+              onClick={prevStepper}
+              className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-lg shadow disabled:opacity-50"
+              disabled={stepper === 1}
+            >
+              Previous
+            </button>
+
+            {
+              stepper < 3 ? <button
+                onClick={nextStepper}
+                className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded-lg shadow disabled:opacity-50"
+              >
+                Next
+              </button> : <input type="button" value=" Submit " className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded-lg shadow disabled:opacity-50 cursor-pointer " />
+            }
+          </div></form>
+
+          {
+            console.log(errors)
           }
-        </div>
 
-
-
-
-
-
-
-
-
-
-
-
-        <div className="flex justify-between items-center w-full px-4 py-2">
-          <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-lg shadow">
-            Previous
-          </button>
-
-          <button className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded-lg shadow">
-            Next
-          </button>
-        </div>
       </div>
     </div>
   );
